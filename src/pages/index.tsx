b5e87c19ffa9d0025d0dd90cdfd2633bca72f2c6
Vaibhav Baria <kiboyagami0@gmail.com>
2023-05-14 21:50:15 -0400

    Getting started

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./index.module.css";
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div className="text-2xl text-red-200"> Mic test</div>
+    </>
+  );
+};
+
+export default Home;
