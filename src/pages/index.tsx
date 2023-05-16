36da18b7b9670fa6a465758021da1125ec587dce
Vaibhav Baria <kiboyagami0@gmail.com>
2023-05-16 00:50:36 -0400

    added random id genretor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,21 @@ import styles from "./index.module.css";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { getOptionsForVote } from "~/utils/getRandomCharID";
 import { trpc } from "~/utils/trpc";
 
 const Home: NextPage = () => {
+
+  const [first, second] = getOptionsForVote();
   return (
     <>
-   
       <div className="h-screen w-screen flex flex-col justify-center items-center">
         <div className="text-2xl text-center">Vote your favourite</div>
         <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
           <div className="p-2" />
-          <div className="w-16 h-16 bg-blue-600"/>
+          <div className="w-16 h-16 bg-blue-600">{first}</div>
           <div className="p-8">Vs</div>
-          
-          <div className="w-16 h-16 bg-blue-600"/> 
+          <div className="w-16 h-16 bg-blue-600"> {second}</div>
         </div>
       </div>
     </>
